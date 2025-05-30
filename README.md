d4faf0782934816ffdb7cd5c778d22fc77cba280
Millo <23leeuw@gmail.com>
2025-05-30 02:52:43 +0200

    m

---
+++ README.md
@@ -0,0 +1,9 @@
+
+# IDX Index
+
+A simplified data api to easily handle keyed items and attributes in Infinite Dungeon X
+
+The goal of this project is to be able to get a simple web request and obtain all required data for items.
+
+To start, run `node .`
+
--- public/index.js
+++ public/index.js
@@ -1,22 +1,37 @@
 import { deserialiseItem } from "./shared/item.js";
+import { deserialiseAttribute } from "./shared/attribute.js";
+import { ItemCard, AttributeCard } from "./src/card.js";
 import DATABASE from "./src/database.js";
 
 const container = document.getElementById("container");
+const selectedDisplay = document.getElementById("selected-display");
 
 DATABASE.getAllItems().then(items => {
-    
     items.forEach(raw => {
         const item = deserialiseItem(raw);
-        console.log(item);
+        const card = new ItemCard(item);
+        container.appendChild(card.toHTML());
     });
 
 })
-// DATABASE.getAllAttributes().then(attributes => {
-//     console.log(attributes);
-// });
+DATABASE.getAllAttributes().then(attributes => {
+    attributes.forEach(raw => {
+        const attribute = deserialiseAttribute(raw);
+        const card = new AttributeCard(attribute);
+        container.appendChild(card.toHTML());
+    })
+});
 
 
-// https://github.com/Owen1212055/mc-assets/blob/main/assets/ACACIA_BOAT.png?raw=true
+const loop = () => {
+    const selected = document.querySelectorAll(".selected");
+    const targetText = selected.length === 0 ? "" : `${selected.length} Selected`;
+    if (selectedDisplay.textContent !== targetText) {
+        selectedDisplay.textContent = targetText;
+    }
 
+    requestAnimationFrame(loop);
+}
+loop();
 
 console.log("hi");
